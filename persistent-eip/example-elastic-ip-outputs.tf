a163f3981e6adf5bd31ce3180d80216a75e8dd75
smjmoj <smjmoj@gmail.com>
2021-03-24 15:13:24 +0000

    Added Persistant EIP as example as possibly needed later

---
+++ persistent-eip/example-elastic-ip-outputs.tf
@@ -0,0 +1,8 @@
+//# outputs
+//
+//output "delius_ndelius_az1_lb_eip" {
+//  value = {
+//    allocation_id = aws_eip.delius_ndelius_az1_lb.id
+//    public_ip     = aws_eip.delius_ndelius_az1_lb.public_ip
+//  }
+//}
