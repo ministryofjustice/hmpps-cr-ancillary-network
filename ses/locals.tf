864bfdb6a98dcc4d7e8216b59f65932a9a2c579e
Chris Kinsella <chris.kinsella@helkinsoft.com>
2021-04-30 13:55:20 +0100

    added ses service

---
+++ ses/locals.tf
@@ -0,0 +1,12 @@
+locals {
+
+  account_id = data.terraform_remote_state.vpc.outputs.vpc_account_id
+  region     = var.region
+  project_name = var.project_name
+
+  strategic_public_zone_id   = data.terraform_remote_state.vpc.outputs.strategic_public_zone_id   
+  strategic_public_zone_name = data.terraform_remote_state.vpc.outputs.strategic_public_zone_name  
+
+  tags = var.tags
+
+}
