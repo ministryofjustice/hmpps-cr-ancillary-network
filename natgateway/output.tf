4e341e4b2daf9164f296230f2c64570f50640163
smjmoj <smjmoj@gmail.com>
2021-05-03 08:02:48 +0100

    Added object and list output format for the natgateway outputs for easier consumption

--- natgateway/output.tf
+++ natgateway/output.tf
@@ -33,3 +33,38 @@ output "natgateway_common-nat-private-ip-az2" {
 output "natgateway_common-nat-private-ip-az3" {
   value = module.common-nat-az3.nat_private_ip
 }
+
+output "this" {
+  value = {
+    az1 = {
+      id         = module.common-nat-az1.natid,
+      private_ip = module.common-nat-az1.nat_private_ip,
+      public_ip  = module.common-nat-az1.nat_public_ip
+    },
+    az2 = {
+      id         = module.common-nat-az2.natid,
+      private_ip = module.common-nat-az2.nat_private_ip,
+      public_ip  = module.common-nat-az2.nat_public_ip
+    },
+    az3 = {
+      id         = module.common-nat-az3.natid,
+      private_ip = module.common-nat-az3.nat_private_ip,
+      public_ip  = module.common-nat-az3.nat_public_ip
+    },
+    ids = [
+      module.common-nat-az1.natid,
+      module.common-nat-az2.natid,
+      module.common-nat-az3.natid
+    ],
+    private_ips = [
+      module.common-nat-az1.nat_private_ip,
+      module.common-nat-az2.nat_private_ip,
+      module.common-nat-az3.nat_private_ip
+    ],
+    public_ips = [
+      module.common-nat-az1.nat_public_ip,
+      module.common-nat-az2.nat_public_ip,
+      module.common-nat-az3.nat_public_ip
+    ]
+  }
+}
