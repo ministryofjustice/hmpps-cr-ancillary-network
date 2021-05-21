5c545a41e77ee9ba175806eb95a784a52989ad05
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-05-21 13:47:39 +0100

    move to use correct refion on output

--- ses/output.tf
+++ ses/output.tf
@@ -3,7 +3,7 @@ output "smtp_credentials" {
     username      = aws_iam_user.ses_user.name
     ssm_param     = aws_ssm_parameter.ses_user_smtppassword.name
     ssm_param_arn = aws_ssm_parameter.ses_user_smtppassword.arn
-    smtp_endpoint = "email-smtp.${var.ses_region}.amazonaws.com"
+    smtp_endpoint = "email-smtp.${var.region}.amazonaws.com"
     smtp_port     = var.ses_smtp_port
     kms_alias_arn = aws_kms_alias.ses_key_alias.arn
   }
