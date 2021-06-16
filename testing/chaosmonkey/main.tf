ba452c499aee96a3f45da150ab7968b903917b05
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-16 10:31:06 +0100

    initial commit for chaosmonkey

---
+++ testing/chaosmonkey/main.tf
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------
+### Getting the current vpc
+#-------------------------------------------------------------
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_state_bucket_name
+    key    = "vpc/terraform.tfstate"
+    region = var.region
+  }
+}
+
+# Set some local compund vars
+locals {
+  name_prefix = "${var.environment_name}-chaosmonkey"
+}
