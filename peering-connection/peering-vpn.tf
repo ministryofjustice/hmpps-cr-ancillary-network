f465fbdce0393932b2fe15cef0dfc4f9097bd058
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-08-04 14:33:54 +0100

    update vpc peeriing routes

--- peering-connection/peering-vpn.tf
+++ peering-connection/peering-vpn.tf
@@ -12,6 +12,66 @@ resource "aws_vpc_peering_connection" "peering-vpn-vpc" {
   )
 }
 
+module "route-to-vpn-vpc-public-cidr-az1" {
+  source = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/routes/vpc_peer?ref=terraform-0.12"
+
+  route_table_id = [
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az3,
+  ]
+
+  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs["vpn-public-az1"]
+  vpc_peer_id            = aws_vpc_peering_connection.peering-vpn-vpc.id
+  create                 = true
+}
+
+module "route-to-vpn-vpc-public-cidr-az2" {
+  source = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/routes/vpc_peer?ref=terraform-0.12"
+
+  route_table_id = [
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az3,
+  ]
+
+  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs["vpn-public-az2"]
+  vpc_peer_id            = aws_vpc_peering_connection.peering-vpn-vpc.id
+  create                 = true
+}
+
+module "route-to-vpn-vpc-public-cidr-az3" {
+  source = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/routes/vpc_peer?ref=terraform-0.12"
+
+  route_table_id = [
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_public-routetable-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_private-routetable-az3,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az1,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az2,
+    data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az3,
+  ]
+
+  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs["vpn-public-az3"]
+  vpc_peer_id            = aws_vpc_peering_connection.peering-vpn-vpc.id
+  create                 = true
+}
+
 # ## Route to VPN private
 
 module "route-to-vpn-vpc-private-cidr-az1" {
@@ -29,7 +89,7 @@ module "route-to-vpn-vpc-private-cidr-az1" {
     data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az3,
   ]
 
-  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs.vpn-private-az1
+  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs["vpn-private-az1"]
   vpc_peer_id            = aws_vpc_peering_connection.peering-vpn-vpc.id
   create                 = true
 }
@@ -49,7 +109,7 @@ module "route-to-vpn-vpc-private-cidr-az2" {
     data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az3,
   ]
 
-  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs.vpn-private-az2
+  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs["vpn-private-az2"]
   vpc_peer_id            = aws_vpc_peering_connection.peering-vpn-vpc.id
   create                 = true
 }
@@ -69,7 +129,8 @@ module "route-to-vpn-vpc-private-cidr-az3" {
     data.terraform_remote_state.vpc.outputs.vpc_db-routetable-az3,
   ]
 
-  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs.vpn-private-az3
+  destination_cidr_block = data.terraform_remote_state.vpn.outputs.subnet_cidrs["vpn-private-az3"]
   vpc_peer_id            = aws_vpc_peering_connection.peering-vpn-vpc.id
   create                 = true
 }
+
