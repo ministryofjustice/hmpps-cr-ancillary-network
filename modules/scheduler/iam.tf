e96100f6b0fd92fa6f3bb7b80c84ac6ebbedd1cf
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-08-13 16:06:06 +0100

    add iam

--- modules/scheduler/iam.tf
+++ modules/scheduler/iam.tf
@@ -1,3 +1,6 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_partition" "current" {}
 data "aws_iam_policy_document" "scheduler_role" {
   statement {
       actions = [
