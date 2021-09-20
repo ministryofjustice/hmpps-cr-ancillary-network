9124c7834e0871315d699cde8af438634de239d6
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-09-20 17:51:28 +0530

    logic to support preprod on same account

--- ses/locals.tf
+++ ses/locals.tf
@@ -2,7 +2,10 @@ locals {
 
   account_id   = data.terraform_remote_state.vpc.outputs.vpc_account_id
   region       = var.region
-  project_name = var.project_name
+
+  # Change in support of deploy mutiple instance of JitBit on same AWS Account with no impact to exiting Infra
+  project_name = var.environment_name != "cr-jitbit-preprod" ? var.project_name : "${var.project_name}-preprod"
+  kms_alias    = var.environment_name != "cr-jitbit-preprod" ? "ses-kms-key" : "preprod-ses-kms-key"
 
   strategic_public_zone_id   = data.terraform_remote_state.vpc.outputs.strategic_public_zone_id
   strategic_public_zone_name = data.terraform_remote_state.vpc.outputs.strategic_public_zone_name
