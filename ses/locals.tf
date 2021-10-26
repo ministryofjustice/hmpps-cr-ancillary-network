12853f99abfaeb5867d845a6d11c914c35d87f0e
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-10-26 17:38:07 +0100

    add jitbit training env

--- ses/locals.tf
+++ ses/locals.tf
@@ -4,9 +4,9 @@ locals {
   region     = var.region
 
   # Change in support of deploy mutiple instance of JitBit on same AWS Account with no impact to exiting Infra
-  env_prefix   = replace("${var.environment_type}", "jitbit-", "")
-  project_name = var.environment_name != "cr-jitbit-dev" ? "${var.project_name}-${local.env_prefix}" : var.project_name
-  kms_alias    = var.environment_name != "cr-jitbit-dev" ? "${local.env_prefix}-ses-kms-key" : "ses-kms-key"
+    env_prefix   = replace("${var.environment_type}", "jitbit-", "")
+  project_name = !contains(["cr-jitbit-dev", "cr-jitbit-prod"], var.environment_name) ? "${var.project_name}-${local.env_prefix}" : var.project_name
+  kms_alias    = !contains(["cr-jitbit-dev", "cr-jitbit-prod"], var.environment_name) ? "${local.env_prefix}-ses-kms-key" : "ses-kms-key"
 
   strategic_public_zone_id   = data.terraform_remote_state.vpc.outputs.strategic_public_zone_id
   strategic_public_zone_name = data.terraform_remote_state.vpc.outputs.strategic_public_zone_name
